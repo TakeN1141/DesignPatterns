dc67216a91241359fc3be8149ec5266e3819cff4
TakeN1141 <59770996+TakeN1141@users.noreply.github.com>
2021-05-06 21:15:27 +0300

    Delete chatRoomHW/src/Mediator directory

--- chatRoomHW/src/Mediator/MediatorMessage.java
+++
@@ -1,9 +0,0 @@
-package Mediator;
-
-public interface MediatorMessage {
-
-    void addUser(User user);
-    void sendMessage(String message, User user);
-    void kickUser(Bot bot, User user, String message);
-
-}
